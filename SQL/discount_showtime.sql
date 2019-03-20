d35bd2d87a0305ab32e56b10ecf55f48f5c84271
suyee97 <43182770+suyee97@users.noreply.github.com>
2019-03-20 17:58:37 +0800

    added "discount_id" into both "showtimes" and "discount_time"

--- SQL/discount_showtime.sql
+++ SQL/discount_showtime.sql
@@ -23,21 +23,23 @@ USE `discount_showtime`;
 
 DROP TABLE IF EXISTS `discount_time`;
 CREATE TABLE IF NOT EXISTS `discount_time` (
+  `discount_id` varchar(13) NOT NULL,
   `discount` decimal(1,1) NOT NULL,
   `time_from` time(0) NOT NULL,
-  `time_to` time(0) NOT NULL
+  `time_to` time(0) NOT NULL,
+  PRIMARY KEY (`discount_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table ``
 --
 
-INSERT INTO `discount_time` (`discount`, `time_from`, `time_to`) VALUES
-('0.1', '20:00:00', '22:59:59'),
-('0.2', '17:00:00', '19:59:59'),
-('0.3', '23:00:00', '08:59:59'),
-('0.4', '09:00:00', '12:59:59'),
-('0.5', '13:00:00', '16:59:59');
+INSERT INTO `discount_time` (`discount_id`,`discount`, `time_from`, `time_to`) VALUES
+('1', '0.1', '20:00:00', '22:59:59'),
+('2', '0.2', '17:00:00', '19:59:59'),
+('3', '0.3', '23:00:00', '08:59:59'),
+('4', '0.4', '09:00:00', '12:59:59'),
+('5', '0.5', '13:00:00', '16:59:59');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
