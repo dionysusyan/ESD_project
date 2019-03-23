18378547780243efeb59977360d5d94d6f427b00
suyee97 <43182770+suyee97@users.noreply.github.com>
2019-03-23 16:33:52 +0800

    Add files via upload

---
+++ SQL/showtimes.sql
@@ -0,0 +1,65 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Jan 14, 2019 at 06:42 AM
+-- Server version: 5.7.19
+-- PHP Version: 7.1.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+
+--
+CREATE DATABASE IF NOT EXISTS `showtimes` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `showtimes`;
+
+-- --------------------------------------------------------
+
+
+
+DROP TABLE IF EXISTS `showtimes`;
+CREATE TABLE IF NOT EXISTS `showtimes` (
+  `sid` varchar(13) NOT NULL,
+  `mid` varchar(13) NOT NULL,
+  `cid` varchar(13) NOT NULL,
+  `discount_id` varchar(13) NOT NULL,
+  `time` time(0) NOT NULL,
+  PRIMARY KEY (`sid`)
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+
+INSERT INTO `showtimes` (`sid`, `mid`,`cid`, `discount_id`, `time`) VALUES
+('1', '1', '1', '5', '13:00:00'),
+('2', '2', '2', '1', '20:00:00'),
+('3', '3', '3', '4', '12:30:00'),
+('4', '4', '4', '4', '10:00:00'),
+('5', '5', '5', '5', '15:30:00'),
+('6', '6', '6', '1', '21:00:00'),
+('7', '1', '2', '3', '23:00:00'),
+('8', '2', '3', '3', '00:30:00'),
+('9', '3', '4', '5', '14:00:00'),
+('10', '4', '5', '2', '17:30:00'),
+('11', '5', '6', '2', '18:00:00'),
+('12', '3', '5', '4', '10:30:00'),
+('13', '3', '5', '5', '14:00:00'),
+('14', '3', '5', '2', '17:30:00'),
+('15', '3', '5', '2', '18:00:00'),
+('16', '3', '5', '1', '20:30:00'),
+('17', '3', '5', '3', '23:30:00');
+
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
